89fd1ecb1c8cd8e4826fd2ece99ab252b5d50e97
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-13 16:53:33 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arr){
+    const new_arr = arr.slice(0, 2);
+    return new_arr;
+}
+
+
+const returnLastTwoDrivers = function(arr){
+    const new_arr = arr.slice(-2);
+    return new_arr;
+}
